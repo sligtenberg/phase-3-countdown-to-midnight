389f658d9f1c0d5750c25a0882587c6ec1542580
stephen ligtenberg <bountyhunter24@gmail.com>
2023-02-13 19:06:18 -0500

    commit tree 93fef0a58aa95f2e8a71081669f93f4f864975f9

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown startNum
+    while startNum > 0
+        puts "#{startNum} SECOND(S)!"
+        startNum -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep startNum
+    while startNum > 0
+        puts "#{startNum} SECOND(S)!"
+        sleep(1)
+        startNum -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
